6e29dc74d045c225f61dc51e2c176e6ae1c945c3
Gogulakumar <113664713+Gogulakumar@users.noreply.github.com>
2022-11-25 17:37:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import './Forms.css'
+import ValidatingForm from './forms';
+
+function App() {
+  return (
+    <div className="App">
+     <ValidatingForm/>
+    </div>
+  );
+}
+
+export default App;
